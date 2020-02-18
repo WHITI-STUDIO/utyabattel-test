b365d7bcce31f61488f74948bcf03fbef82902d7
WHITI-STUDIO <61202267+WHITI-STUDIO@users.noreply.github.com>
2020-02-18 19:57:53 +0200

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1582048431,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"sprite-sheet0.png",
+		"sprite2-sheet0.png",
+		"sprite3-sheet0.png",
+		"sprite4-sheet0.png",
+		"sprite5-sheet0.png",
+		"sprite6-sheet0.png",
+		"sprite7-sheet0.png",
+		"sprite8-sheet0.png",
+		"sprite9-sheet0.png",
+		"sprite10-sheet0.png",
+		"sprite11-sheet0.png",
+		"sprite12-sheet0.png",
+		"sprite13-sheet0.png",
+		"sprite14-sheet0.png",
+		"sprite15-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
